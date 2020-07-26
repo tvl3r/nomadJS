cd7eb8a2c56ca56d31d8d4dc056b15bd7aa0942d
JihyeongAn <zpfjqps@gmail.com>
2020-07-26 15:01:31 +0900

    Fisrt commit

---
+++ greeting.js
@@ -0,0 +1,44 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input");
+
+const greeting = document.querySelector(".js-greetings"),
+  SHOWING_CN = "showing";
+
+const USER_LS = "currentUser";
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  saveName(currentValue);
+  paintGreeting(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
